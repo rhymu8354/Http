d99f25250cc7ebfad4bec1ebff0c17533d8921c6
Richard Walters <rwalters@digitalstirling.com>
2018-08-28 15:07:04 -0700

    Fix bad test
    ResponseTests::GenerateGetRequest: make the response actually
    HTTP compliant by including a Content-Length header.

--- test/src/ResponseTests.cpp
+++ test/src/ResponseTests.cpp
@@ -18,13 +18,18 @@ TEST(ResponseTests, GenerateGetRequest) {
     response.headers.AddHeader("Accept-Ranges", "bytes");
     response.headers.AddHeader("Content-Type", "text/plain");
     response.body = "Hello World! My payload includes a trailing CRLF.\r\n";
+    response.headers.AddHeader("Content-Length", SystemAbstractions::sprintf("%zu", response.body.size()));
     ASSERT_EQ(
-        "HTTP/1.1 200 OK\r\n"
-        "Date: Mon, 27 Jul 2009 12:28:53 GMT\r\n"
-        "Accept-Ranges: bytes\r\n"
-        "Content-Type: text/plain\r\n"
-        "\r\n"
-        "Hello World! My payload includes a trailing CRLF.\r\n",
+        SystemAbstractions::sprintf(
+            "HTTP/1.1 200 OK\r\n"
+            "Date: Mon, 27 Jul 2009 12:28:53 GMT\r\n"
+            "Accept-Ranges: bytes\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: %zu\r\n"
+            "\r\n"
+            "Hello World! My payload includes a trailing CRLF.\r\n",
+            response.body.size()
+        ),
         response.Generate()
     );
 }
